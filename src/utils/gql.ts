b549a4aecb49c9781b2d1674418ac8949659903e
John Letey <johnletey@gmail.com>
2020-08-27 14:37:58 +0100

    feat: Add helper function for Arweave GQL endpoint

---
+++ src/utils/gql.ts
@@ -0,0 +1,17 @@
+// Client for the Arweave GraphQL endpoint
+
+export async function query({ query, variables = null }) {
+  var graphql = JSON.stringify({
+    query,
+    variables,
+  });
+  var requestOptions = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: graphql,
+  };
+  let res = await fetch("https://arweave.dev/graphql", requestOptions);
+  return await res.clone().json();
+}
