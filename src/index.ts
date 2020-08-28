ec94af4f606c3f845125ed8d4bcfa48770b21e12
John Letey <johnletey@gmail.com>
2020-08-28 09:59:20 +0100

    feat: Check for genesis tx

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { init } from "@utils/arweave";
 import { query } from "@utils/gql";
 import Log from "@utils/logger";
+import genesisQuery from "./queries/genesis.gql";
 import txsQuery from "./queries/txs.gql";
 
 const log = new Log({
@@ -15,16 +16,33 @@ bootstrap().catch((err) => console.log(err));
 async function bootstrap() {
   const { client, community } = await init();
 
+  const possibleGenesis = (
+    await query({
+      query: genesisQuery,
+      variables: {
+        // @ts-ignore
+        owners: ["l-x7026roC1dkAmJ5iWhz4vGOxVnKmotGbceFAA-NwE"],
+        // @ts-ignore
+        reciepents: ["pvPWBZ8A5HLpGSEfhEmK1A3PfMgB_an8vVS6L14Hsls"],
+      },
+    })
+  ).data.transactions.edges;
+  if (possibleGenesis.length === 1) {
+    log.info(`Genesis tx found: ${possibleGenesis[0].node.id}`);
+  } else {
+    log.info("No genesis found!");
+  }
+
   // This logic grabs the latest trade tx
-  console.log(
-    (
-      await query({
-        query: txsQuery,
-        variables: {
-          // @ts-ignore
-          num: 1,
-        },
-      })
-    ).data.transactions.edges[0]
-  );
+  // console.log(
+  //   (
+  //     await query({
+  //       query: txsQuery,
+  //       variables: {
+  //         // @ts-ignore
+  //         num: 1,
+  //       },
+  //     })
+  //   ).data.transactions.edges[0]
+  // );
 }
