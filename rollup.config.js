70123bb46871a0b7a857d69af5d3b16e0b42b29b
divy-work <dj.srivastava23@gmail.com>
2020-08-28 10:53:54 +0530

    feat: use rollup with gql loader

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+// rollup.config.js
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+import { createFilter } from "@rollup/pluginutils";
+import resolve from "@rollup/plugin-node-resolve";
+import alias from "@rollup/plugin-alias";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
+const filter = createFilter("**/*.gql", []);
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+  ],
+  external: [...Object.keys(pkg.dependencies || {}), "*.gql"],
+  plugins: [
+    typescript(),
+    json(),
+    resolve(),
+    alias({
+      "@utils": __dirname + "/src/utils",
+    }),
+    {
+      name: "string",
+      transform(code, id) {
+        if (filter(id)) {
+          return {
+            code: `export default ${JSON.stringify(code)};`,
+            map: { mappings: "" },
+          };
+        }
+      },
+    },
+    commonjs(),
+  ],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import "module-alias/register";
 import { query } from "@utils/gql";
 import Log from "@utils/logger";
+import bootstrapQuery from "./queries/bootstrap.gql";
 
 const log = new Log({
   level: Log.Levels.debug,
@@ -14,17 +14,7 @@ bootstrap().catch((err) => console.log(err));
 async function bootstrap() {
   console.log(
     await query({
-      query: `
-    query {
-      transactions(tags: [{ name: "Exchange", values: "Verto" }]) {
-        edges {
-          node {
-            id
-          }
-        }
-      }
-    }
-  `,
+      query: bootstrapQuery,
     })
   );
 }
