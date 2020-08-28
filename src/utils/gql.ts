6d7fda3d95a633eb6b4df3e235a47de37ab83a77
divy-work <dj.srivastava23@gmail.com>
2020-08-28 11:06:19 +0530

    chores(docs): add doc comment to query executer

--- src/utils/gql.ts
+++ src/utils/gql.ts
@@ -1,15 +1,29 @@
 // Client for the Arweave GraphQL endpoint
 import fetch from "node-fetch";
 
-interface GrapqlQuery {
-  query: string;
-  variables?: string;
+interface StringMap {
+  [key: string]: string;
 }
-export async function query({ query, variables }: GrapqlQuery) {
-  var graphql = JSON.stringify({
-    query,
-    variables,
-  });
+
+/**
+ * Represents a graphql query
+ */
+interface GrapqlQuery {
+  /**
+   * The graphql query as a string
+   */
+  query: string;
+  /**
+   * The graphql variables in the given query.
+   */
+  variables?: string | StringMap;
+}
+
+/**
+ * Perform a HTTP request to the graphql server.
+ * @param graphql The response body as string
+ */
+async function request(graphql: string) {
   var requestOptions = {
     method: "POST",
     headers: {
@@ -19,4 +33,28 @@ export async function query({ query, variables }: GrapqlQuery) {
   };
   let res = await fetch("https://arweave.dev/graphql", requestOptions);
   return await res.clone().json();
+}
+
+/**
+ * Execute a graphql query with variables.
+ * @param param0 A graphql query and its vaiables.
+ */
+export async function query({ query, variables }: GrapqlQuery) {
+  var graphql = JSON.stringify({
+    query,
+    variables,
+  });
+  return await request(graphql);
+}
+
+/**
+ * Execute a simple graphql query without variables.
+ * @param query The graphql query to be executed.
+ */
+export async function simpleQuery(query: string) {
+  var graphql = JSON.stringify({
+    query,
+    variables: {},
+  });
+  return await request(graphql);
 }
