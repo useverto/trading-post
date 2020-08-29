95d96f7465d336397975c90c46661e3c1b55424f
divy-work <dj.srivastava23@gmail.com>
2020-08-29 13:19:11 +0530

    feat: add koa

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,31 @@ import commander from "commander";
 import Log from "@utils/logger";
 import { init } from "@utils/arweave";
 import { genesis } from "workflows/genesis";
+import Koa from "koa";
+
+const http = new Koa();
+
+// logger
+
+http.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  log.debug(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+http.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+
+http.use(async ctx => {
+  ctx.body = 'Hello World';
+});
 
 const log = new Log({
   level: Log.Levels.debug,
@@ -39,3 +64,5 @@ program.parse(process.argv);
 if (program.keyFile && program.config) {
   bootstrap(program.keyFile, program.config).catch((err) => log.error(err));
 }
+
+app.listen(3000);
