8ff902359d98ad97876621754478cda8202b5b59
John Letey <johnletey@gmail.com>
2020-08-30 12:01:51 +0100

    chore: Update genesis logs

--- src/workflows/genesis.ts
+++ src/workflows/genesis.ts
@@ -33,7 +33,9 @@ export async function genesis(
     })
   ).data.transactions.edges;
   if (possibleGenesis.length === 1) {
-    log.info(`genesis_tx_id=${possibleGenesis[0].node.id}`);
+    log.info(
+      `Found genesis transaction.\n\t\ttxId=${possibleGenesis[0].node.id}`
+    );
   } else {
     log.info("Sending genesis transaction");
     const tx = await client.createTransaction(
