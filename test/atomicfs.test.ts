e43f19a9f795e31f3027d319d4eeeae1bf52b906
divy-work <dj.srivastava23@gmail.com>
2020-08-30 16:26:20 +0530

    chores: remove atomic tests are no catchup is needed

--- test/atomicfs.test.ts
+++
@@ -1,9 +0,0 @@
-import { writeFileAtomic } from "../src/utils/atomicfs";
-
-const file = Buffer.allocUnsafe(1024 * 1024); // 1MB
-
-describe("Atomic fs tests", () => {
-  it("write atomic fs", async () => {
-    return await writeFileAtomic("test_artifacts/xyz", file);
-  });
-});
