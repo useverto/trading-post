d0245c0317aa5cfaffe0dc5c99f0ff5f61ff811c
divy-work <dj.srivastava23@gmail.com>
2020-08-31 18:27:28 +0530

    fix: tests

--- test/database.test.ts
+++ test/database.test.ts
@@ -2,6 +2,6 @@ import { init } from "../src/utils/database";
 
 describe("Database tests", () => {
   it("Init database", async () => {
-    return await init("./test_artifacts/db");
+    return await init("./test_artifacts/db.db");
   });
 });
