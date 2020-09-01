d6ee09fd0eeddee8117860f057e77af824031ae7
John Letey <johnletey@gmail.com>
2020-09-01 08:56:38 +0100

    feat: Implement more match cases

--- src/workflows/match.ts
+++ src/workflows/match.ts
@@ -35,7 +35,7 @@ export async function match(
   // TODO(@johnletey): Update some of these tags once frontend is updated
   const opcode = tx.tags.find((tag: any) => tag.name === "Trade-Opcode")
     ?.value!;
-  const amnt = tx.tags.find((tag: any) => tag.name === "Amnt")?.value!;
+  let amnt = tx.tags.find((tag: any) => tag.name === "Amnt")?.value!;
   const token = tx.tags.find((tag: any) => tag.name === "Target-Token")?.value!;
   const rate = tx.tags.find((tag: any) => tag.name === "Rate")?.value!;
 
@@ -47,6 +47,7 @@ export async function match(
     rate,
     addr: tx.owner.address,
     type: opcode,
+    createdAt: new Date(),
   };
   await saveOrder(db, token, tokenEntry);
 
@@ -124,28 +125,50 @@ export async function match(
           amnt - order.amnt / order.rate,
           txId,
         ]);
+        amnt -= order.amnt / order.rate;
         await db.run(`DELETE FROM "${token}" WHERE txID = ?`, [order.txID]);
       }
     }
   } else if (opcode === "Sell") {
     const orders = await getBuyOrders(db, token);
     for (const order of orders) {
-      // If buy order is greater than sell order worth
-      if (order.amnt > amnt / rate) {
-        //    Send PST to buyer
-        //    Send AR to seller
-        //    Remove sell order from DB
-        //    Keep buy order but subtract amount from sell order's worth
+      if (order.amnt >= amnt / rate) {
+        const arTx = await client.createTransaction(
+          {
+            target: tx.owner.address,
+            quantity: client.ar.arToWinston((amnt / rate).toString()),
+          },
+          jwk
+        );
+        await client.transactions.sign(arTx, jwk);
+        await client.transactions.post(arTx);
+
+        const pstTx = await interactWrite(
+          client,
+          jwk,
+          token,
+          `{"function": "transfer", "target": "${order.addr}", "qty": ${amnt}}`
+        );
+
+        // TODO(@johnletey): Add logs
+
+        if (order.amnt === amnt / rate) {
+          await db.run(`DELETE FROM "${token}" WHERE txID = ?`, [order.txID]);
+        } else {
+          await db.run(`UPDATE "${token}" SET amnt = ? WHERE txID = ?`, [
+            order.amnt - amnt / rate,
+            order.txID,
+          ]);
+        }
+        await db.run(`DELETE FROM "${token}" WHERE txID = ?`, [txId]);
+
+        return;
         // If buy order is less than sell order worth
-      } else if (order.amnt < amnt / rate) {
+      } else {
         //    Fill buy order
         //    Partially fill sell order
         //    Remove buy order from DB
         //    Keep sell order but subtract amount from buy order's worth
-        // Both orders are equal
-      } else {
-        // Fill both buy and sell orders
-        // Remove both orders from the DB
       }
     }
   } else {
