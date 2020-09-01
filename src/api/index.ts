64da1b98adb61e10f84ecfe00e1440dfb8b8c90d
divy-work <dj.srivastava23@gmail.com>
2020-09-01 15:15:14 +0530

    chores: rm unwanted log

--- src/api/index.ts
+++ src/api/index.ts
@@ -33,7 +33,6 @@ export function initAPI(host?: string, port?: number, db?: Database) {
   port = port || 8080;
   host = host || "localhost";
   const verifyID = v4();
-  log.debug(verifyID);
   http.use(createRouter(db).routes());
   http.listen(port, host);
   log.debug(`Started trading post server at port ${port}`);
