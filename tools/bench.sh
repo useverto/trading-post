eb5dc7384fdd9a39d297c1691c530be3c87ba3a5
divy-work <dj.srivastava23@gmail.com>
2020-09-03 12:03:47 +0530

    feat: remove redundant tools

--- tools/bench.sh
+++
@@ -1,8 +0,0 @@
-# Build to standalone production js
-yarn prod
-
-# Build standalone production binaries
-yarn pkg
-
-# Run benchmarks on both
-hyperfine 'node ./dist/verto.js' './verto' -i --export-markdown ./BENCHMARKS.md --warmup 5
