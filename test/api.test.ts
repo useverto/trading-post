04ebdc7aa0eaf7f251eedcdeffdfb8ca323c0bf3
divy-work <dj.srivastava23@gmail.com>
2020-09-03 11:09:11 +0530

    feat: complete API tests

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,28 +1,25 @@
-import {
-    initAPI
-  } from "../src/api/index";
-  import supertest, { SuperTest, Test } from "supertest";
-  let server: any;
-  let request: SuperTest<Test>;
+import { initAPI } from "../src/api/index";
+import supertest, { SuperTest, Test } from "supertest";
+let server: any;
+let request: SuperTest<Test>;
 
-  describe("API tests", () => {
-    it("Start server", (done) => {
-        server = initAPI("localhost", 8080).listen();
-        request = supertest(server);
-        done();
-    });
-    it("Test server response", (done) => {
-        request
-        .get('/ping')
-        .expect(200)
-        .end((err, res) => {
-            if(err) return done(err);
-            done();
-        })
-    });
-    it("Shutdown server", (done) => {
-        server.close();
-        done();
-    });
+describe("API tests", () => {
+  it("Start server", (done) => {
+    server = initAPI("localhost", 8080, undefined, false).listen();
+    request = supertest(server);
+    done();
   });
-  
+  it("Test server response", (done) => {
+    request
+      .get("/ping")
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+  it("Shutdown server", (done) => {
+    server.close();
+    done();
+  });
+});
