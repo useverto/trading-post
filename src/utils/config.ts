0baebfe8eca1f206f97a656e80647492da3bbc93
divy-work <dj.srivastava23@gmail.com>
2020-09-05 15:54:32 +0530

    feat: add config validation

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -20,7 +20,7 @@ export interface APIConfig {
  */
 export interface GenesisConfig {
   acceptedTokens: string[];
-  tradeFee: string;
+  tradeFee: number;
   version: string;
   publicURL: URL | string;
 }
@@ -35,6 +35,30 @@ export interface TradingPostConfig {
 }
 
 /**
+ * Basically a gtfo-if-not-valid utility for validating config
+ * @param msg message to log while validating the verto configuration
+ */
+const logValidate = (msg: string) => {
+  log.error(`Config validation failed: ${msg}`);
+  process.exit(1);
+};
+
+/**
+ * The trading post config that needs to be validated at runtime.
+ * @param obj A don't-know-if-valid trading post configuration
+ */
+export function validateConfig(obj: TradingPostConfig) {
+  obj.genesis.acceptedTokens &&
+    !obj.genesis.acceptedTokens.every((i) => typeof i === "string") &&
+    logValidate("tokens must be string and nothing else");
+  obj.genesis.tradeFee &&
+    typeof obj.genesis.tradeFee !== "number" &&
+    logValidate("trade free must be of a valid integer");
+  typeof obj.genesis.tradeFee !== "number" &&
+    logValidate("database location must be a string");
+}
+
+/**
  * Read the trading post config
  * @param loc Location of the config file
  */
@@ -43,6 +67,7 @@ export async function loadConfig(loc: string): Promise<TradingPostConfig> {
     let config: TradingPostConfig = JSON.parse(
       await readFile(loc, { encoding: "utf8" })
     );
+    validateConfig(config);
     log.info(`Loaded config file from ${loc}`);
     return config;
   } catch (e) {
