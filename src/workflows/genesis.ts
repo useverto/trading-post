631f3f6d9b3872afa52feb598ceebe3c7d485fcb
John Letey <johnletey@gmail.com>
2020-09-06 09:27:44 +0100

    chore: Remove wip code from genesis workflow

--- src/workflows/genesis.ts
+++ src/workflows/genesis.ts
@@ -12,17 +12,6 @@ const log = new Log({
   name: "genesis",
 });
 
-// function isEqual(a: any, b: any) {
-//   if (a === b) return true;
-//   if (a instanceof Date && b instanceof Date) return a.getTime() === b.getTime();
-//   if (!a || !b || (typeof a !== 'object' && typeof b !== 'object')) return a === b;
-//   if (a === null || a === undefined || b === null || b === undefined) return false;
-//   if (a.prototype !== b.prototype) return false;
-//   let keys = Object.keys(a);
-//   if (keys.length !== Object.keys(b).length) return false;
-//   return keys.every(k => isEqual(a[k], b[k]));
-// };
-
 export async function genesis(
   client: Arweave,
   community: Community,
@@ -56,15 +45,6 @@ export async function genesis(
     log.info(
       `Found genesis transaction.\n\t\ttxId = ${possibleGenesis[0].node.id}`
     );
-
-    // const currentConfig = JSON.parse(
-    //   (
-    //     await client.transactions.getData(possibleGenesis[0].node.id, {
-    //       decode: true,
-    //       string: true,
-    //     })
-    //   ).toString()
-    // );
 
     return possibleGenesis[0].node.id;
   } else {
