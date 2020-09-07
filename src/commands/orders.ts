4c980beec7812242f8ffe0a73582326b73a1a389
divy-work <dj.srivastava23@gmail.com>
2020-09-07 16:21:51 +0530

    feat: add passing config

--- src/commands/orders.ts
+++ src/commands/orders.ts
@@ -1,8 +1,13 @@
 import { init, collectDatabase, TokenInstance } from "@utils/database";
 import { asTree } from "@utils/tree";
+import { loadConfig } from "@utils/config";
 
-export default async () => {
-  const connection = await init("./db.db");
+/**
+ * Display the trading post order book
+ */
+export default async (opts: any) => {
+  let cnf = await loadConfig(opts.config)
+  const connection = await init(cnf.database);
   let orders = await collectDatabase(connection);
   orders = orders.filter((i) => i.table !== "__verto__");
   let orderTree: {
