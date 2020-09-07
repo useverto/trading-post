5d1d2ceae6e8d883d8a0bba448c4c3edd5be5f46
divy-work <dj.srivastava23@gmail.com>
2020-09-07 16:16:53 +0530

    feat: complete order command

---
+++ src/commands/orders.ts
@@ -0,0 +1,15 @@
+import { init, collectDatabase, TokenInstance } from "@utils/database";
+import { asTree } from "@utils/tree";
+
+export default async () => {
+    const connection = await init("./db.db");
+    let orders = await collectDatabase(connection);
+    orders = orders.filter(i => i.table !== "__verto__");
+    let orderTree: {
+        [token: string]: TokenInstance[]
+    } = {};
+    orders.forEach(element => {
+        orderTree[element.table] = element.data;
+    });
+    console.log(asTree(orderTree))
+}
