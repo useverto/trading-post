db3c27969dd4b7faec3555f22fe7779fc988453a
divy-work <dj.srivastava23@gmail.com>
2020-09-10 22:27:45 +0530

    chores: fmt

--- test/commands.test.ts
+++ test/commands.test.ts
@@ -1,9 +1,9 @@
 import OrdersCommand from "../src/commands/orders";
 
 describe("Test `verto orders`", () => {
-    it("Retrieve order book", async () => {
-        return await OrdersCommand({
-            config: "verto.config.example.json",
-        })
+  it("Retrieve order book", async () => {
+    return await OrdersCommand({
+      config: "verto.config.example.json",
     });
+  });
 });
