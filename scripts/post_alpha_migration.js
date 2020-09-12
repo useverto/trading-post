e270a7d1c2edf7190fba1580feb51ecdad08403d
divy-work <dj.srivastava23@gmail.com>
2020-09-12 14:52:10 +0530

    feat: complete migration script

--- scripts/post_alpha_migration.js
+++ scripts/post_alpha_migration.js
@@ -1,22 +1,34 @@
 const { open, Database } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
-const sqlite = await open({
+async function collectDatabase(db) {
+  let tables = await db.all(
+    "SELECT name FROM sqlite_master WHERE type='table'"
+  );
+  let columns = tables.map(async (table) => {
+    return {
+      table: table.name,
+      data: await db.all(`SELECT * FROM "${table.name}"`),
+    };
+  });
+
+  let data = await Promise.all(columns);
+  return data;
+}
+
+async function main() {
+  const sqlite = await open({
     filename: "db.db",
     driver: sqlite3.Database,
-});
+  });
 
-async function collectDatabase(db) {
-    let tables = await db.all(
-      "SELECT name FROM sqlite_master WHERE type='table'"
+  let dbTables = await collectDatabase(sqlite);
+  dbTables = dbTables.filter((i) => i.table !== "__verto__");
+  dbTables.forEach(async (table) => {
+    await sqlite.exec(
+      `ALTER TABLE "${table.table}" ADD COLUMN received INTEGER NOT NULL DEFAULT 0;`
     );
-    let columns = tables.map(async (table) => {
-      return {
-        table: table.name,
-        data: await db.all(`SELECT * FROM "${table.name}"`),
-      };
-    });
-  
-    let data = await Promise.all(columns);
-    return data;
-  }
+  });
+}
+
+main().catch(console.error);
