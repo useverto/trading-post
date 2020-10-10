238e2103f18a9610fc5dde849e0e9e63b27b7449
Divy Srivastava <dj.srivastava23@gmail.com>
2020-10-10 16:46:39 +0000

    feat(tests): make extendible

--- test/cli.go
+++ test/cli.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "log"
-    "os/exec"
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
 )
+
+const startupText = "VERTO INTEGRATION TEST SUITE 1.0"
+
+var logger = TestLogger{}
 
 type Commands struct {
 	command string
-	
 }
-const ShellToUse = "bash"
 
 func Shellout(command string) (error, string, string) {
-    var stdout bytes.Buffer
-    var stderr bytes.Buffer
-    cmd := exec.Command(ShellToUse, "-c", command)
-    cmd.Stdout = &stdout
-    cmd.Stderr = &stderr
-    err := cmd.Run()
-    return err, stdout.String(), stderr.String()
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(ShellToUse, "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return err, stdout.String(), stderr.String()
 }
 
 func main() {
-    err, out, errout := Shellout("./verto")
-    if err != nil {
-        log.Printf("error: %v\n", err)
-    }
-    fmt.Println("--- stdout ---")
-    fmt.Println(out)
-    fmt.Println("--- stderr ---")
-    fmt.Println(errout)
+	fmt.Println(startupText)
+	err, out, _ := Shellout("./verto orders -c verto.config.example.json")
+	if err != nil {
+		log.Printf("error: %v\n", err)
+	}
+	logger.Success(out)
 }
