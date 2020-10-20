fb1689f1467413f47267b15e620cd8bc4c13f892
divy-work <dj.srivastava23@gmail.com>
2020-10-20 10:51:27 +0530

    feat: trading post startup

--- test/integration/spin.go
+++ test/integration/spin.go
@@ -3,14 +3,24 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 )
 
 func StartTradingPost() {
+	ch := make(chan bool, 1)
 	command := "./verto -c verto.config.example.json -k arweave.json"
-	err, stderr, _ := Shellout(command)
-	if err != nil {
-		log.Printf("error: %v\n", stderr)
-		os.Exit(1)
+	go func() {
+		err, stderr, _ := Shellout(command)
+		if err != nil {
+			log.Printf("error: %v\n", stderr)
+			os.Exit(1)
+		}
+		Logger.Success(command)
+		ch <- true
+	}()
+	select {
+	case <-ch:
+	case <-time.After(1 * time.Millisecond):
+		Logger.Success(command)
 	}
-	Logger.Success(command)
 }
