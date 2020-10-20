1d93a36338a4be209f4adc76d8b93e752e5fdc5e
divy-work <dj.srivastava23@gmail.com>
2020-10-20 16:47:10 +0530

    chores: fmt

--- test/integration/log.go
+++ test/integration/log.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-const _brightGreen = "\u001b[32;1m";
-const _reset = "\u001b[0m";
+const _brightGreen = "\u001b[32;1m"
+const _reset = "\u001b[0m"
 
-type TestLogger struct {}
+type TestLogger struct{}
 
 func (logger *TestLogger) Success(out string) {
-    fmt.Println(_brightGreen, "OK", _reset, "Pass", out)
+	fmt.Println(_brightGreen, "OK", _reset, "Pass", out)
 }
