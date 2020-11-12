b632b6060222df8d9005ab25bf153d564020f9c8
John Letey <johnletey@gmail.com>
2020-11-12 21:08:36 +0000

    feat: initial `/pairs` endpoint logic

--- src/api/gecko/pairs.ts
+++ src/api/gecko/pairs.ts
@@ -1,22 +1,86 @@
+import { GQLEdgeInterface } from "types";
 import { query } from "@utils/gql";
-import tokensQuery from "../../queries/tokens.gql";
-import { init } from "@utils/arweave";
-import CONSTANTS from "../utils/constants.yml";
+import Arweave from "arweave";
+
+const getAll = async () => {
+  let hasNextPage = true;
+  let edges: GQLEdgeInterface[] = [];
+  let cursor: string = "";
+
+  while (hasNextPage) {
+    const res = (
+      await query({
+        query: `
+        query {
+          transactions(
+            tags: [
+              { name: "Exchange", values: "Verto" }
+              { name: "Type", values: ["Buy", "Sell"] }
+            ]
+            after: "${cursor}"
+          ) {
+            pageInfo {
+              hasNextPage
+            }
+            edges {
+              cursor
+              node {
+                id
+                tags {
+                  name
+                  value
+                }
+              }
+            }
+          }
+        }      
+      `,
+      })
+    ).data.transactions;
+
+    if (res.edges && res.edges.length) {
+      edges = edges.concat(res.edges);
+      cursor = res.edges[res.edges.length - 1].cursor;
+    }
+    hasNextPage = res.pageInfo.hasNextPage;
+  }
+
+  return edges;
+};
 
 export async function getPairs() {
-  // Query for all PSTs
-  // AR_PST
-
-  const currencies = await query({
-    query: tokensQuery,
-    variables: {
-      exchange: CONSTANTS.exchangeWallet,
-    },
+  const client = new Arweave({
+    host: "arweave.net",
+    port: 443,
+    protocol: "https",
   });
 
-  return {
-    ticker_id: "BTC_ETH",
-    base: "BTC",
-    target: "ETH",
-  };
+  const tokens: {
+    ticker_id: string;
+    base: string;
+    target: string;
+  }[] = [];
+  const res = await getAll();
+  for (const entry of res) {
+    const type = entry.node.tags.find((tag) => tag.name === "Type")?.value;
+    const token = entry.node.tags.find(
+      (tag) => tag.name === (type === "Buy" ? "Token" : "Contract")
+    )?.value!;
+
+    if (tokens.find((entry) => entry.ticker_id === token)) continue;
+
+    const buf = await client.transactions.getData(token, {
+      decode: true,
+      string: true,
+    });
+    const contract = JSON.parse(buf.toString());
+
+    tokens.push({
+      ticker_id: token,
+      base: "AR",
+      target: contract.ticker,
+    });
+  }
+
+  return tokens;
 }
