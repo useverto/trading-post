9c90f5deb396ba74be7e83290c81679e35679676
divy-work <dj.srivastava23@gmail.com>
2020-11-12 19:23:25 +0530

    chores(windows): update installer uri

--- e2e/install.rb
+++ e2e/install.rb
@@ -4,6 +4,6 @@ def install
 		if OS.unix?
 			exec_cmd('curl -fsSL http://localhost:3000/install/linux.sh | sh')
 		else
-			exec_cmd('iwr https://verto.exchange/i/windows | iex')
+			exec_cmd('iwr http://localhost:3000/install/windows.ps1 | iex')
 		end
 end
