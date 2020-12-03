71540a3bcf32d153e1810e5ad2b2fa4a94304e06
John Letey <johnletey@gmail.com>
2020-12-03 22:11:26 +0000

    feat: add migrate script

---
+++ scripts/migrate.js
@@ -0,0 +1,32 @@
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+async function collectTables(db) {
+  let tables = await db.all(
+    "SELECT name FROM sqlite_master WHERE type='table'"
+  );
+
+  return tables
+    .map((table) => {
+      return table.name;
+    })
+    .filter((table) => table !== "__verto__")
+    .filter((table) => table !== "TX_STORE");
+}
+
+async function main() {
+  const sqlite = await open({
+    filename: "db.db",
+    driver: sqlite3.Database,
+  });
+
+  let tables = await collectTables(sqlite);
+
+  tables.forEach(async (table) => {
+    await sqlite.exec(
+      `ALTER TABLE "${table}" ADD COLUMN token STRING;`
+    );
+  });
+}
+
+main().catch(console.error);
